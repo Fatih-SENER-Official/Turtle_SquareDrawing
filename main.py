382984a8e8d852b0bddde4a0f7aead9fe562364a
Fatih SENER <fatihsener840@gmail.com>
2024-07-21 15:18:06 +0300

    Turtle Square Drawing

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("#55FF33")
+drawing_board.title("Python Turtle")
+# square is drawn
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance_2 = turtle.Turtle()
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
+turtle_instance_2.right(90)
+turtle_instance_2.forward(100)
+turtle_instance_2.right(90)
+turtle_instance_2.forward(100)
+turtle.done
+
